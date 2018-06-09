34655cb84392ce1c308085e4145ef40b2bdc0da9
e_ntyo <entyo.contact@gmail.com>
2018-06-09 18:05:28 +0900

    Add noodle page

---
+++ hosting/pages/call/noodle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="root-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>麺の量・硬さの指定</span>
+        <el-button style="float: right; padding: 3px 0" type="text">着席した！</el-button>
+      </div>
+      <div class="text">
+        食券を購入し列に並んでいると、店員さんに食券の色を聞かれます。<br>
+        麺の量を減らしたり固めにしたりしてもらうときは、このタイミングでお願いしましょう。<br>
+        席に着いたら、"着席した! "を押してください。
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.root-container {
+  padding: 2.5em;
+}
+</style>
